4c4c6815c833eee41976383e009317320764eed8
winnie <winnie@email>
2022-10-04 21:39:53 +0900

    mysql

--- mysql_node/app.js
+++ mysql_node/app.js
@@ -5,11 +5,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var client = mysql.createConnection({
-    user: 'mysql',
-    password: 'mysql',
-    host: '192.168.0.14',
+    user: 'lch',
+    password: 'lch',
+    host: '127.0.0.1',
     port: '3306',
-    database: 'sample_db'
+    database: 'node_data'
 })
 
 var app = express();
@@ -34,8 +34,38 @@ app.get('/', function (req, res) {
         });
     })
 })
-app.get('/delete/:id', function (req, res) {})
-app.get('/insert', function (req, res) {})
-app.post('/insert', function (req, res) {})
-app.get('/edit/:id', function (req, res) {})
-app.post('/edit/:id', function (req, res) {})
+app.get('/delete/:id', function (req, res) {
+    client.query('delete from products where id=?', [req.params.id], function () {
+        res.redirect('/')
+    })
+})
+app.get('/insert', function (req, res) {
+    fs.readFile('insert.html', 'utf8', function (error, data) {
+        res.send(data);
+    })
+})
+app.post('/insert', function (req, res) {
+    var body = req.body;
+
+    client.query('insert into products (name, modelnumber, series) values(?,?,?)', [body.name, body.modelnumber, body.series], function () {
+        res.redirect('/');
+    })
+})
+app.get('/edit/:id', function (req, res) {
+    fs.readFile('edit.html', 'utf8', function (error, data) {
+        client.query('select * from products where id = ?', [req.params.id], function (error, result) {
+            console.log(result);
+            res.send(ejs.render(data, {
+                data: result[0]
+            }))
+        })
+    })
+})
+app.post('/edit/:id', function (req, res) {
+    var body = req.body;
+
+    client.query('update products set name=?, modelnumber=?, series=? where id=?', [body.name, body.modelnumber, body.series, req.params.id],
+        function () {
+            res.redirect('/')
+        })
+})
