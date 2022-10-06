c4364acd6072c27fac6b0e2f7e0e1785af286d52
winnie <winnie@email>
2022-10-06 11:17:24 +0900

    express

--- second_node/jade.basic.js
+++ second_node/jade.basic.js
@@ -5,11 +5,16 @@ var jade = require('jade');
 http.createServer(function (request, response) {
 
         fs.readFile('jadePage.jade', 'utf8', function (error, data) {
+            
+            var paramVal = {
+                name: 'Rinta12345',
+                desc: 'Hello World'
+            }
             var fn = jade.compile(data);
             response.writeHead(200, {
                 'Content-type': 'text/html'
             })
-            response.end(fn());
+            response.end(fn(paramVal));
         })
     })
     .listen(52273, function () {
