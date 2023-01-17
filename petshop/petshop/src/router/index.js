b2050aa95ea74ed0ce6cfd7cd500356c46513595
ubuntu <ubuntu@email.com>
2023-01-17 17:14:06 +0900

    first

---
+++ petshop/petshop/src/router/index.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Form from '@/components/Form'
+import Main from '@/components/Main'
+import Product from '@/components/Product'
+import EditProduct from '@/components/EditProduct'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [{
+      path: '/',
+      name: 'iMain',
+      component: Main,
+      props: true
+    },
+    {
+      path: '/form',
+      name: 'Form',
+      component: Form,
+      props: true
+    },
+    {
+      path: '/product/:id',
+      name: 'Id',
+      component: Product,
+      props: true,
+      children: [{
+        path: 'edit',
+        name: 'Edit',
+        component: EditProduct,
+        props: true
+      }]
+    },
+    {
+      path: '/hello',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '*',
+      redirect: '/'
+    }
+  ]
+})
