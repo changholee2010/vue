4919d58ccfe936b2e69873cc09ba4ff14c28a475
you <you@email.com>
2025-06-19 23:14:15 +0900

    for rest

--- vue-project/vue-proj/src/App.vue
+++ vue-project/vue-proj/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/databinding">Etc</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ vue-project/vue-proj/src/views/DataBindingCheck.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>
+        <label>{{ radio1 }}</label>
+        <input type="radio" v-model="radio1" v-bind:value="radio1">
+    </p>
+</template>
+
+<script>
+    export defaut {
+        data() {
+            return {
+                radio1: true,
+                radio2: false,
+                radio3: false,
+            }
+        },
+    }
+</script>
