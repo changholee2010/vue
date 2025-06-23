36dff6f0f76da5f35dabe744954d7aca6466c4ee
home <home@email.com>
2025-06-24 01:08:43 +0900

    for push

--- vue-project/vue-proj/src/router/index.js
+++ vue-project/vue-proj/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DataBindingView from '../views/ParentComponent4.vue'
+import DataBindingView from '../views/ParentComponent5.vue'
 
 const routes = [
     {
