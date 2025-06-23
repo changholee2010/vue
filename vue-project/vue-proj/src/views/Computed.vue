23ba4b5f5ade485e5a317d0de637b1657b84c40d
you <you@email.com>
2025-06-23 17:46:19 +0900

    today is done

---
+++ vue-project/vue-proj/src/views/Computed.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>FullName: {{ fullName }}</h1>
+    <h3>FullName: {{ firstName + ' ' + lastName }}</h3>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            firstName: 'kildong',
+            lastName: 'Hong',
+        }
+    },
+    computed: {
+        fullName() {
+            return this.firstName + ' ' + this.lastName
+        },
+    },
+}
+</script>
