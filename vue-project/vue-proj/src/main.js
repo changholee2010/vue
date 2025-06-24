22aa794cdf2011d74e97a1ac2f35937432cbe11e
you <you@email.com>
2025-06-24 14:43:49 +0900

    mmm

--- vue-project/vue-proj/src/main.js
+++ vue-project/vue-proj/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import mixins from './mixins'
 
 const app = createApp(App)
@@ -10,5 +11,6 @@ app.directive('focus', {
     },
 })
 app.use(router)
+app.use(store)
 app.mixin(mixins)
 app.mount('#app')
---
+++ vue-project/vue-proj/src/views/StoreAccess.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>Count: {{ count }}</p>
+    <p>CartCnt: {{ cartCount }}</p>
+    <button type="button" v-on:click="increment">Increment</button>
+</template>
+
+<script>
+export default {
+    computed: {
+        count() {
+            return this.$store.state.count
+        },
+        cartCount() {
+            return this.$store.getters.cartCount
+        },
+    },
+    methods: {
+        increment() {
+            this.$store.commit('increment')
+        },
+    },
+}
+</script>
