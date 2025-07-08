e46dd4c550be1c51aa438d7a5d468829942504de
changholee2010 <148027761+changholee2010@users.noreply.github.com>
2025-07-08 11:13:01 +0900

    Delete vue_js directory

--- vue_js/Vue_QuickStart/07_ECMAScript2015/src/07-20.js
+++
@@ -1,4 +0,0 @@
-import calc from './utils/util3';
-
-console.log(calc.add(4, 5));
-console.log(calc.multiply(4, 5));
--- vue_js/vue/board02/components/main.js
+++
@@ -1,6 +0,0 @@
-export default {
-    template: `
-    <div>
-        <h3>게시판을 불러와주세요</h3>
-    </div>`
-}
